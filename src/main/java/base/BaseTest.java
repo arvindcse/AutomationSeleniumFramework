042651daa99ac379cde514170b78d8db3423391d
arvindcse <arvindcse@users.noreply.github.com>
2025-09-17 16:45:09 +0530

    commit

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,19 +1,27 @@
 package base;
 
+import utils.Log;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 public class BaseTest {
 
 	protected WebDriver driver;
+	
+	
 
 	@BeforeMethod
 	public void setUp() {
+		
+		Log.info("Starting WebDriver...");
 		driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		
+		Log.info("Navigate to URL....");
 		
 		driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
 	
@@ -21,7 +29,10 @@ public class BaseTest {
 	
 	@AfterMethod
 	public void tearDown() {
+		
 		if(driver != null) {
+			
+			Log.info("Closing Browser....");
 		//	driver.quit();
 		}
 		
